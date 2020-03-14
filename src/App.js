27e499c6be0e6a473894fbaa4ecc50823284a7e4
Giulio Giraldi <dongiulio@gmail.com>
2020-03-14 16:13:17 +0100

    including bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* global fetch */
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -20,8 +20,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header container">
+        <h1>COVID-19 test</h1>
         <p>{message}</p>
         <p>Change me!</p>
         <p>
