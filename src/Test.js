a116b9102c49b8a2caab40a0eaa0c34736a04bcd
Giulio Giraldi <dongiulio@gmail.com>
2020-03-21 17:59:55 +0100

    calculating total points for test

--- src/Test.js
+++ src/Test.js
@@ -2,26 +2,52 @@ import React from 'react';
 import Question from "./Question";
 
 import {Button} from 'reactstrap';
-const Test = (props) => {
-  return(
-    <div>
-    {
-      Object.values(props.questions).map((question, index) => 
-                  <div>
-                    <Question key={index+1} 
-                              question={question["question"]} 
-                              answers={question["answers"]} 
-                              index={index + 1}/>
-                    <hr/>
-                  </div>
-                              )
 
-    }
-
-    <Button color="primary" type="submit">Results</Button>
-    </div>
-
+function makeTotal(questions) {
+  var total = 0;
+  
+  Object.values(questions).map((question) => 
+    Object.values(question.answers).map((answer) => {
+      return total += answer.value }
     )
+  );
+  return total;
+}
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: makeTotal(props.questions),
+      current: 0
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('total was: ' + this.state.total);
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        {
+          Object.values(this.props.questions).map((question, index) => 
+            <div key={index+1}>
+              <Question question={question["question"]} 
+                        answers={question["answers"]} 
+                        index={index + 1}/>
+              <hr/>
+            </div>)
+        }
+        <Button color="primary" type="submit">Results</Button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Test;
