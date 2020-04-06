b0b5edb12a4e612af59087c0b08874c1ae332b37
Giulio Giraldi <dongiulio@gmail.com>
2020-04-06 23:20:39 +0200

    removing duplicate intro copy

--- src/Test.js
+++ src/Test.js
@@ -4,23 +4,13 @@ import { useTranslation } from 'react-i18next';
 import questions from "./questions";
 import categories from "./categories";
 import DoTest from "./DoTest";
-import Description from "./Description";
-import Language from "./Language";
 
 const Test = () => {
   const [t] = useTranslation();
 
   return (
     <div className="Test">
-      <header className="Test-header container">
-        <h1>{ t('common:main.title') }</h1>
-      </header>
-        <hr/>
-        <Language />
-        <Description />
-        <hr/>
         <h1>{ t('common:main.test.title') }</h1>
-
         <DoTest questions={questions} categories={categories}/>
     </div>
 
